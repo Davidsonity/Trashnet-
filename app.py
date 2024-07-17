b36e1c36bd91964dfc2d76ae92bb50cb90972dbd
Davidsonity <eshemitanvoke@gmail>
2024-07-17 04:01:15 +0100

    Update

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.image import img_to_array, load_img
+from tensorflow.keras.preprocessing.image import img_to_array
 import numpy as np
 from PIL import Image
 
@@ -22,27 +22,38 @@ def preprocess_image(image):
     return image
 
 # Streamlit app
-st.title('Image Classification with Machine Learning')
+st.title('Solid Waste Classification with Using Deep Learning')
 st.subheader('By Emuejevoke Eshemitan')
-st.image('web_image.jpeg')
 
-st.write('Upload an image to classify it using the trained model.')
+# Display the university logo
+
+st.image('image1.png', caption='University of Lagos', use_column_width=True)
+
+st.info('Please use images with a white background for better accuracy as the model was trained on images with a white background.')
 
 # File uploader
 uploaded_file = st.file_uploader('Choose an image...', type=['jpg', 'jpeg', 'png'])
 
+# Camera input
+camera_image = st.camera_input('Take a picture')
+
+image = None
 if uploaded_file is not None:
-    # Load and preprocess the image
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Image', use_column_width=True)
-    st.write('Classifying...')
+elif camera_image is not None:
+    image = Image.open(camera_image)
+    st.image(image, caption='Captured Image', use_column_width=True)
+
+if image is not None:
     image = preprocess_image(image)
 
     # Make a prediction
-    if model:
-        prediction = model.predict(image)
-        predicted_class = labels[np.argmax(prediction)]
-        # Display the prediction
-        st.write(f'Predicted class: {predicted_class}')
-    else:
-        st.error("Model could not be loaded.")
+    with st.spinner('Classifying...'):
+        if model:
+            prediction = model.predict(image)
+            predicted_class = labels[np.argmax(prediction)]
+            # Display the prediction
+            st.success(f'Predicted class: {predicted_class}')
+        else:
+            st.error("Model could not be loaded.")
